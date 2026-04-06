ccc59e6e224f184c654761a59544c9ea3f19b312
mnishanthkumar12 <mnishanthkumar9@gmail.com>
2026-04-07 00:48:52 +0530

    yes committed

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks and also we did ."""
+    """Run all cases ."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faq_project.settings')
     try:
         from django.core.management import execute_from_command_line
